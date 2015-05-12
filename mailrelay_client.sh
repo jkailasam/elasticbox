3420d740b79bcc70019e205cb8616ecfd629378e
Jeeva Kailasam <jkailasam@netflix.com>
2015-05-12 16:08:11 -0700

    adding postfix files

---
+++ mailrelay_client.sh
@@ -0,0 +1,154 @@
+    echo "10.201.0.0/24 vpc-2bfc1f4e us-east-1c seg_us-east-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.1.0/24 vpc-2bfc1f4e us-east-1d seg_us-east-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.10.0/24 vpc-2bfc1f4e us-east-1e seg_us-east-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.104.0/24 vpc-c45344a6 us-west-1a prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.105.0/24 vpc-c45344a6 us-west-1c prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.106.0/24 vpc-c45344a6 us-west-1a prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.112.0/24 vpc-b38f90d1 us-west-2a seg_us-west-2 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.113.0/24 vpc-b38f90d1 us-west-2b seg_us-west-2 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.114.0/24 vpc-b38f90d1 us-west-2c seg_us-west-2 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.120.0/24 vpc-b38f90d1 us-west-2a seg_us-west-2 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.121.0/24 vpc-b38f90d1 us-west-2b seg_us-west-2 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.122.0/24 vpc-b38f90d1 us-west-2c seg_us-west-2 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.128.0/24 vpc-d7180ab5 us-west-2a dev_us-west-2 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.129.0/24 vpc-d7180ab5 us-west-2b dev_us-west-2 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.130.0/24 vpc-d7180ab5 us-west-2c dev_us-west-2 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.136.0/24 vpc-d7180ab5 us-west-2a dev_us-west-2 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.137.0/24 vpc-d7180ab5 us-west-2b dev_us-west-2 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.138.0/24 vpc-d7180ab5 us-west-2c dev_us-west-2 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.144.0/24 vpc-a48f90c6 us-west-2a prod_us-west-2 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.145.0/24 vpc-a48f90c6 us-west-2b prod_us-west-2 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.146.0/24 vpc-a48f90c6 us-west-2c prod_us-west-2 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.152.0/24 vpc-a48f90c6 us-west-2a prod_us-west-2 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.153.0/24 vpc-a48f90c6 us-west-2b prod_us-west-2 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.154.0/24 vpc-a48f90c6 us-west-2c prod_us-west-2 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.2.0/24 vpc-2bfc1f4e us-east-1e seg_us-east-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.32.0/24 vpc-802f34e2 us-east-1c dev_us-east-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.33.0/24 vpc-802f34e2 us-east-1d dev_us-east-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.34.0/24 vpc-802f34e2 us-east-1e dev_us-east-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.40.0/24 vpc-802f34e2 us-east-1c dev_us-east-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.41.0/24 vpc-802f34e2 us-east-1d dev_us-east-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.42.0/24 vpc-802f34e2 us-east-1e dev_us-east-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.48.0/24 vpc-eadd388f us-east-1c prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.49.0/24 vpc-eadd388f us-east-1d prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.50.0/24 vpc-eadd388f us-east-1e prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.56.0/24 vpc-eadd388f us-east-1c prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.57.0/24 vpc-eadd388f us-east-1d prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.58.0/24 vpc-eadd388f us-east-1e prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.64.0/24 vpc-ffbfa99d us-west-1a seg_us-west-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.65.0/24 vpc-ffbfa99d us-west-1c seg_us-west-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.66.0/24 vpc-ffbfa99d us-west-1a seg_us-west-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.72.0/24 vpc-ffbfa99d us-west-1a seg_us-west-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.73.0/24 vpc-ffbfa99d us-west-1c seg_us-west-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.74.0/24 vpc-ffbfa99d us-west-1a seg_us-west-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.8.0/24 vpc-2bfc1f4e us-east-1c seg_us-east-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.80.0/24 vpc-eed3c78c us-west-1a dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.81.0/24 vpc-eed3c78c us-west-1c dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.82.0/24 vpc-eed3c78c us-west-1a dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.88.0/24 vpc-eed3c78c us-west-1a dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.89.0/24 vpc-eed3c78c us-west-1c dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.9.0/24 vpc-2bfc1f4e us-east-1d seg_us-east-1 mailrelay.its.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.90.0/24 vpc-eed3c78c us-west-1a dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.96.0/24 vpc-c45344a6 us-west-1a prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.97.0/24 vpc-c45344a6 us-west-1c prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.201.98.0/24 vpc-c45344a6 us-west-1a prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.202.0.0/22 vpc-5c70b939 us-east-1c prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.202.14.0/24 vpc-5c70b939 us-east-1c prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.202.15.0/24 vpc-5c70b939 us-east-1d prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.202.4.0/22 vpc-5c70b939 us-east-1d prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.203.0.0/24 vpc-2c99864e us-west-1a prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "10.203.1.0/28 vpc-94cfd3f6 us-west-1a dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.16.0.0/24 vpc-b2ae6bd7 us-east-1d prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.16.1.0/24 vpc-b2ae6bd7 us-east-1e prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.16.2.0/24 vpc-b2ae6bd7 us-east-1d prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.16.3.0/24 vpc-b2ae6bd7 us-east-1e prod_us-east-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.17.0.0/24 vpc-6f4da00a us-west-2b prod_us-west-2 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.17.1.0/24 vpc-6f4da00a us-west-2c prod_us-west-2 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.17.2.0/24 vpc-6f4da00a us-west-2b prod_us-west-2 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.17.3.0/24 vpc-6f4da00a us-west-2c prod_us-west-2 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.18.0.0/24 vpc-519f8133 us-west-1c prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.18.1.0/24 vpc-519f8133 us-west-1a prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.18.2.0/24 vpc-519f8133 us-west-1c prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.18.3.0/24 vpc-519f8133 us-west-1a prod_us-west-1 mailrelay.itp.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.26.0.0/24 vpc-03c73a66 us-east-1c dev_us-east-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.26.1.0/24 vpc-03c73a66 us-east-1d dev_us-east-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.26.2.0/24 vpc-03c73a66 us-east-1c dev_us-east-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.26.3.0/24 vpc-03c73a66 us-east-1d dev_us-east-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.27.0.0/24 vpc-2bff1e4e us-west-2a dev_us-west-2 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.27.1.0/24 vpc-2bff1e4e us-west-2b dev_us-west-2 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.27.2.0/24 vpc-2bff1e4e us-west-2a dev_us-west-2 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.27.3.0/24 vpc-2bff1e4e us-west-2c dev_us-west-2 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.28.0.0/24 vpc-bbb3aed9 us-west-1a dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.28.1.0/24 vpc-bbb3aed9 us-west-1c dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.28.2.0/24 vpc-bbb3aed9 us-west-1a dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+    echo "172.28.3.0/24 vpc-bbb3aed9 us-west-1c dev_us-west-1 mailrelay.itd.netflix.com" >> /tmp/mailrelay_lookup_table
+
+
+   ACCTIP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk -F. '{ print $1"."$2"."$3}'`
+   MAILRELAYHOST=`grep $ACCTIP /tmp/mailrelay_lookup_table | awk '{print $5}'|uniq`
+   DOMAIN=`echo $MAILRELAYHOST | awk -F"." '{printf ("%s.%s.%s\n", $2,$3, $4)}'`
+   DEFAULT_NETWORKS="127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128"
+
+hostname | grep mr10
+if [ $? -eq 0 ]; then
+    MYNETWORKS="$DEFAULT_NETWORK `grep $MAILRELAYHOST /tmp/mailrelay_lookup_table | awk '{printf ("%s ", $1)}'` "
+else
+    MYNETWORKS="$DEFAULT_NETWORK"
+fi
+    echo MYNETWORKS: $MYNETWORKS
+    echo MAILRELAY:  $MAILRELAY
+
+    echo MYNETWORKS: $MYNETWORKS >> /etc/status
+
+    postconf -e smtpd_banner="\\$myhostname ESMTP \\$mail_name (Ubuntu)"
+    postconf -e biff="no"
+    postconf -e append_dot_mydomain="no"
+    postconf -e readme_directory="no"
+
+    # Give the host a compliant name
+    postconf -e myhostname="`hostname -f`"
+    postconf -e alias_maps="hash:/etc/aliases"
+    postconf -e alias_database="hash:/etc/aliases"
+
+    # Disable local mail delivery. All mail goes to the mail server as specified in relayhost
+    postconf -e mydestination=""
+
+    if [ ! -f /etc/postfix/externa-mail-relay ]; then
+        postconf -e relayhost="${MAILRELAYHOST}:25"
+    fi
+
+    # Only temporary
+    # postconf -e relayhost="mail.netflix.com"
+    mkdir /root/work
+    cat <<! > /root/work/mail-test-script.sh
+    echo "mail from \`hostname -f\` on \`date\` sent to myusuf@netflix.com" | mailx -s"mail from \`hostname\` on \`date\`" myusuf@netflix.com
+!
+
+    postconf -e mynetworks="$MYNETWORKS"
+    postconf -e mailbox_size_limit=0
+    postconf -e recipient_delimiter="+"
+    postconf -e inet_interfaces="all"
+    postconf -e relay_domains="netflix.com"
+    postconf -e myorigin="$DOMAIN"
+    postconf -e inet_protocols="ipv4"
+
+    # re-write the header so that root@domain is sent to the SA distribution list
+    postconf -e smtp_generic_maps="hash:/etc/postfix/generic"
+    #echo "root@${DOMAIN} myusuf@netflix.com" > /etc/postfix/generic
+    echo "root@${DOMAIN} it-cloud-engineering@netflix.com" > /etc/postfix/generic
+    postmap /etc/postfix/generic
+
+    echo "root:    it-cloud-engineering@netflix.com" >> /etc/aliases
+    newaliases
+
+    # Insert the proper name in /etc/hosts
+    ifconfig eth0 | awk '/inet addr/ {print $2}' | awk -F: '{print $2}' | xargs -i sed -i 's/127.0.1.1/{}/' /etc/hosts
+
+    ## Reduce the email chattiness of Elasticbox from once a minute to once every fifteen minutes
+    echo '0 7 * * * root PATH=/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin; /usr/elasticbox/elasticbox start | grep -v "ElasticBox agent was already running"' > /etc/cron.d/elasticbox
+    service cron restart
+    service postfix restart
+
+    exit 0
+}
+
